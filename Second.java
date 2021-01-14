f2a42d5bb854cbdb55fde7ffc8ae6122477ee364
Yevdochenko <77458373+Yevdochenko@users.noreply.github.com>
2021-01-14 22:23:49 +0200

    Update Second.java

--- Second.java
+++ Second.java
@@ -8,13 +8,15 @@ public class Second {
 
         int catYears = 0;
         int dogYears = 0;
+        
         if(humanYears>=1){
             catYears += 15;
             dogYears += 15;
-
+            
             if(humanYears>=2){
                 catYears += 9;
                 dogYears += 9;
+                
                 if(humanYears==humanYears){
                     catYears += (humanYears-2)*4;
                     dogYears += (humanYears-2)*6;
