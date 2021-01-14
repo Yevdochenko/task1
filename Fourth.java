f2faaaf8de32527d36f122d7bbb6630e9a089674
Yevdochenko <77458373+Yevdochenko@users.noreply.github.com>
2021-01-14 23:07:00 +0200

    Update Fourth.java

--- Fourth.java
+++ Fourth.java
@@ -1,8 +1,17 @@
 public class Fourth {
 
     public static void main(String[] arge){
-        String str = "This is just an example string for test";
-        String result = str.toUpperCase();
-        System.out.println("Result: " + result);
+        String str1 = "This is just an example string for test";
+        //String result = str1.toUpperCase();
+        String str2 = "";
+        str2 = str2 + str1.substring(0, 1).toUpperCase();
+        for (int i = 1; i < str1.length(); i++) {
+            if (" ".equals(str1.substring(i-1, i)))
+                str2 = str2 + str1.substring(i, i+1).toUpperCase();
+            else
+                str2 = str2 + str1.substring(i, i+1);
+        }
+
+        System.out.println("Result: " + str2);
     }
 }
