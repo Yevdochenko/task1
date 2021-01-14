fb771e497f9c98d7cd24fefdebd4b7172d5d8fc5
Yevdochenko <77458373+Yevdochenko@users.noreply.github.com>
2021-01-14 22:26:13 +0200

    Update Second.java

--- Second.java
+++ Second.java
@@ -5,18 +5,14 @@ public class Second {
         Scanner in = new Scanner(System.in);
         System.out.printf("HumanYears: ");
         int humanYears = in.nextInt();
-
         int catYears = 0;
         int dogYears = 0;
-        
         if(humanYears>=1){
             catYears += 15;
             dogYears += 15;
-            
             if(humanYears>=2){
                 catYears += 9;
                 dogYears += 9;
-                
                 if(humanYears==humanYears){
                     catYears += (humanYears-2)*4;
                     dogYears += (humanYears-2)*6;
